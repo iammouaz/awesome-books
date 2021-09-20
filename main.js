f77c58dbcb6a699157a3c3134f700916d0d5cf55
Ahmed <ahmdadl563@gmail.com>
2021-09-20 22:54:11 +0200

    Push booj list details to Show in DOM

--- main.js
+++ main.js
@@ -9,27 +9,26 @@ function voks (){
 }
 
 const submit = document.getElementById('submit');
-submit .addEventListener('click', function (){
+submit.addEventListener('click', function (){
     let newBook = new voks;
     newBook.title = document.getElementById('title').value;
     newBook.author = document.getElementById('author').value;
-    bookList.push(newBook);
-    for (let i = 0; i<= bookList.length; i++){
+    console.log(newBook.title);
+    console.log(newBook.author)
         let contain = document.createElement('div');
         let title = document.createElement('div');
         title.id = 'title';
         let author = document.createElement('div');
         author.id = 'author';
-        title.textContent = bookList[i].title;
-        author.textContent = bookList[i].author;
+        title.textContent = newBook.title;
+        author.textContent = newBook.author;
         let remove = document.createElement('button');
-        remove.type = 'button';
         remove.textContent = 'Remove';
         contain.appendChild(title);
         contain.appendChild(author);
         contain.appendChild(remove);
         showField.appendChild(contain);
-    }
+        bookList.push(newBook);
 })
 
 

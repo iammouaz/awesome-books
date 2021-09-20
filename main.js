05dfb997b1dbe770d3c3c4427060e05574879a37
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-20 20:44:10 +0300

    Modify the add funcation

--- main.js
+++ main.js
@@ -11,11 +11,15 @@ submit .addEventListener('click', function (){
     newBook.title = document.getElementById('title').value;
     newBook.author = document.getElementById('author').value;
     bookList.push(newBook);
+    console.log(newBook);
 })
 
-const bookTitle = document.getElementById('title');
-const bookAuthor = document.getElementById('author');
+const bookTitle = document.getElementById('bookTitle');
+const bookAuthor = document.getElementById('bookAuthor');
+bookList.forEach((item) => {
+    bookTitle.innerHTML =+ `<span>${item.title}</span>`
+});
 
-for (let i = 0; i<bookList.length; i++){
-    bookAuthor.innerHTML =  
-}
+// for (let i = 0; i<bookList.length; i++){
+//     bookAuthor.innerHTML =  
+// }

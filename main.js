2febe3acd5dfc0f0d0ccc55424337515de93179a
Ahmed <ahmdadl563@gmail.com>
2021-09-20 23:14:58 +0200

    I am trying

--- main.js
+++ main.js
@@ -15,20 +15,20 @@ submit.addEventListener('click', function (){
     newBook.author = document.getElementById('author').value;
     console.log(newBook.title);
     console.log(newBook.author)
-        let contain = document.createElement('div');
-        let title = document.createElement('div');
-        title.id = 'title';
-        let author = document.createElement('div');
-        author.id = 'author';
-        title.textContent = newBook.title;
-        author.textContent = newBook.author;
-        let remove = document.createElement('button');
-        remove.textContent = 'Remove';
-        contain.appendChild(title);
-        contain.appendChild(author);
-        contain.appendChild(remove);
-        showField.appendChild(contain);
-        bookList.push(newBook);
+    let contain = document.createElement('div');
+    let title = document.createElement('div');
+    title.id = 'title';
+    let author = document.createElement('div');
+    author.id = 'author';
+    title.textContent = newBook.title;
+    author.textContent = newBook.author;
+    let remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    contain.appendChild(title);
+    contain.appendChild(author);
+    contain.appendChild(remove);
+    showField.appendChild(contain);
+    bookList.push(newBook);
 })
 
 

25661ddc6c2f7a4b37067bc108658ae00902f29e
Ahmed <ahmdadl563@gmail.com>
2021-09-21 16:49:57 +0200

    Linters Done

--- main.js
+++ main.js
@@ -9,50 +9,48 @@ function Voks(title, author) {
 }
 
 function addBook() {
-    let newBook = new Voks();
-    newBook.title = document.getElementById('title').value;
-    newBook.author = document.getElementById('author').value;
-    bookList.push(newBook);
+  const newBook = new Voks();
+  newBook.title = document.getElementById('title').value;
+  newBook.author = document.getElementById('author').value;
+  bookList.push(newBook);
 }
 
-function pushContent(){
-    document.getElementById('books-list').innerHTML = '';
-    document.getElementById('title').value ='';
-    document.getElementById('author').value = '';
-    bookList.forEach((e)=>{
-        let contain = document.createElement('div');
-        contain.className = 'contain'
-        let title = document.createElement('div');
-        title.className = 'title';
-        let author = document.createElement('div');
-        author.className = 'author';
-        title.innerHTML = e.title;
-        author.innerHTML = e.author;
-        let remove = document.createElement('button');
-        remove.className = 'remove';
-        remove.textContent = 'Remove';
-        
-        contain.appendChild(title);
-        contain.appendChild(author);
-        contain.appendChild(remove);
-        showField.appendChild(contain);
-    });
+function pushContent() {
+  document.getElementById('books-list').innerHTML = '';
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  bookList.forEach((e) => {
+    const contain = document.createElement('div');
+    contain.className = 'contain';
+    const title = document.createElement('div');
+    title.className = 'title';
+    const author = document.createElement('div');
+    author.className = 'author';
+    title.innerHTML = e.title;
+    author.innerHTML = e.author;
+    const remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'Remove';
+
+    contain.appendChild(title);
+    contain.appendChild(author);
+    contain.appendChild(remove);
+    showField.appendChild(contain);
+  });
 }
 
 function removeBook() {
-    let removBtn = document.querySelectorAll('.remove');
-    for (let i = 0; i<removBtn.length; i++){
-        removBtn[i].addEventListener('click', ()=>{
-             bookList.splice(bookList[removBtn[i]],1);
-            removBtn[i].parentNode.remove();
-            console.log(bookList)
-        })
-
-    }
+  const removBtn = document.querySelectorAll('.remove');
+  for (let i = 0; i < removBtn.length; i += 1) {
+    removBtn[i].addEventListener('click', () => {
+      bookList.splice(bookList[removBtn[i]], 1);
+      removBtn[i].parentNode.remove();
+    });
+  }
 }
 
-submit.addEventListener('click', ()=>{
-    addBook();
-    pushContent();
-    removeBook();
+submit.addEventListener('click', () => {
+  addBook();
+  pushContent();
+  removeBook();
 });

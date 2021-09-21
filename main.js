0f92335eafeaf39db4c30f53be34b3232fd5c710
Ahmed <ahmdadl563@gmail.com>
2021-09-21 12:59:08 +0200

    Now Add books functionality Done

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 const showField = document.querySelector('.books-list');
-const bookTitle = document.getElementById('bookTitle');
-const bookAuthor = document.getElementById('bookAuthor');
+
 let bookList = [];
 
-function voks (){
+function Voks(title,author){
     this.title = title;
     this.author = author;
 }
-
 const submit = document.getElementById('submit');
-submit.addEventListener('click', function (){
-    let newBook = new voks;
+
+function addBook(){
+    let newBook = new Voks();
     newBook.title = document.getElementById('title').value;
     newBook.author = document.getElementById('author').value;
-    console.log(newBook.title);
-    console.log(newBook.author)
-    let contain = document.createElement('div');
-    let title = document.createElement('div');
-    title.id = 'title';
-    let author = document.createElement('div');
-    author.id = 'author';
-    title.textContent = newBook.title;
-    author.textContent = newBook.author;
-    let remove = document.createElement('button');
-    remove.textContent = 'Remove';
-    contain.appendChild(title);
-    contain.appendChild(author);
-    contain.appendChild(remove);
-    showField.appendChild(contain);
     bookList.push(newBook);
-})
+}
 
+function pushContent(){
+    document.getElementById('books-list').innerHTML = '';
+    document.getElementById('title').value ='';
+    document.getElementById('author').value = '';
+    bookList.forEach((e)=>{
+        let contain = document.createElement('div');
+        contain.id = 'contain'
+        let title = document.createElement('div');
+        title.className = 'title';
+        let author = document.createElement('div');
+        author.className = 'author';
+        title.innerHTML = e.title;
+        author.innerHTML = e.author;
+        let remove = document.createElement('button');
+        remove.textContent = 'Remove';
+        
+        contain.appendChild(title);
+        contain.appendChild(author);
+        contain.appendChild(remove);
+        showField.appendChild(contain);
+    });
+}
 
-bookList.forEach((item) => {
-    console.log(bookList['voks']);
+submit.addEventListener('click', ()=>{
+    addBook();
+    pushContent();
 });
-
-// for (let i = 0; i<bookList.length; i++){
-//     bookAuthor.innerHTML =  
-// }

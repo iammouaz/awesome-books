ff474ac1028215f17a076743fe66bff0df2df022
Ahmed <ahmdadl563@gmail.com>
2021-09-23 16:01:22 +0200

    Fixing js linter

--- main.js
+++ main.js
@@ -70,14 +70,14 @@ window.onload = () => {
 // Single page phase
 
 const linkContent = document.querySelectorAll('.add-new, .contact');
-linkContent.forEach((e) => {e.style.display = 'none'});
+linkContent.forEach((e) => { e.style.display = 'none'; });
 
 const link = document.querySelectorAll('a');
 const sections = document.querySelectorAll('section');
 
 for (let i = 0; i < link.length; i += 1) {
   link[i].addEventListener('click', () => {
-    sections.forEach((e) => {e.style.display = 'none'});
+    sections.forEach((e) => { e.style.display = 'none'; });
     sections[i].style.display = 'flex';
   });
 }
